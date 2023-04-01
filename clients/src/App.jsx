b48816ac6ca3e9f2e0764f8679555135f1a52c4d
Sakshi Ingle <64880576+sakshi68@users.noreply.github.com>
2023-04-02 00:13:29 +0530

    Add files via upload

---
+++ clients/src/App.jsx
@@ -0,0 +1,17 @@
+import {Navbar,Welcome,Transactions,Services,Footer,Loader} from "./components";
+
+
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
+    </div>
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
+);
+
+export default App;
+ 
